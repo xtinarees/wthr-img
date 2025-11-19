cb52b198de7f172ec7e1b5a34aa3f8efbc6f9ff2
Christina Rees <reesxtina@gmail.com>
2025-11-18 22:09:33 -0600

    better control panel

--- src/components/BackgroundControls.jsx
+++ src/components/BackgroundControls.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import tinycolor from 'tinycolor2';
+import React from "react";
+import tinycolor from "tinycolor2";
 
 /**
- * Manage background styles for controls panel 
+ * Manage background styles for controls panel
  * inside of div in order to control opacity independently
-
  */
 const BackgroundControls = ({ color }) => {
   const colorStr = tinycolor(color).toString();
-  // const gradient = `linear-gradient(to right, rgba(255,0,0,0), ${colorStr})`;
   const gradient = `linear-gradient(to right, rgba(255,0,0,0), ${colorStr}, ${colorStr})`;
 
-  const stylesDesktop = { backgroundImage: gradient };
-  const stylesMobile = { backgroundColor: colorStr };
+  const styles = { backgroundImage: gradient };
 
   return (
     <div className="controls__background">
-      <div className="controls__background-inner controls__background--mobile" style={stylesMobile}></div>
-      <div className="controls__background-inner controls__background--desktop" style={stylesDesktop}></div>
+      <div className="controls__background-inner" style={styles}></div>
     </div>
   );
 };
