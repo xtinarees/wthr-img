8a7e2838dfb49ea9be40cf148461754024d2dd1f
Christina Rees <reesxtina@gmail.com>
2025-11-18 22:28:42 -0600

    prettier + smaller preceip on mobile

--- src/components/Precipitation.jsx
+++ src/components/Precipitation.jsx
@@ -1,24 +1,25 @@
-import React, { useMemo } from 'react';
-import tinycolor from 'tinycolor2';
-import { getRandomInt } from '../utils';
-
+import React, { useMemo } from "react";
+import tinycolor from "tinycolor2";
+import { getRandomInt } from "../utils";
 
 function PrecipitationItem({ types, precipType, color }) {
   // Memoize the drops so they don't change on every render unless types/precipType changes
   const drops = useMemo(() => {
-    return Array(50).fill(1).map((el, i) => {
-      const sizeCSS = getRandomInt(2, 20) + 'px';
-      const topCSS = getRandomInt(0, 100) + 'vh';
-      const leftCSS = getRandomInt(0, 100) + 'vw';
-      const styles = {
-        width: sizeCSS,
-        height: sizeCSS,
-        top: topCSS,
-        left: leftCSS,
-        backgroundColor: precipType === "rainy" ? color : "white"
-      };
-      return <div className={precipType} style={styles} key={i}></div>;
-    });
+    return Array(50)
+      .fill(1)
+      .map((el, i) => {
+        const sizeCSS = getRandomInt(2, 20) + "px";
+        const topCSS = getRandomInt(0, 100) + "vh";
+        const leftCSS = getRandomInt(0, 100) + "vw";
+        const styles = {
+          width: `clamp(0px, ${sizeCSS}, 3vw)`,
+          height: `clamp(0px, ${sizeCSS}, 3vw)`,
+          top: topCSS,
+          left: leftCSS,
+          backgroundColor: precipType === "rainy" ? color : "white",
+        };
+        return <div className={precipType} style={styles} key={i}></div>;
+      });
   }, [types, precipType]);
 
   return <>{drops}</>;
@@ -30,12 +31,15 @@ function Precipitation({ types, color }) {
     <>
       {types.map((precipType) => (
         <div className="precipitation" key={precipType}>
-          <PrecipitationItem types={types} precipType={precipType} color={backgroundColor}/>
+          <PrecipitationItem
+            types={types}
+            precipType={precipType}
+            color={backgroundColor}
+          />
         </div>
       ))}
     </>
   );
 }
-
 
 export default Precipitation;
